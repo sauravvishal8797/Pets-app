d2f42e611c55c64c16c48c6dedbf36666b95aff7
Saurav Vishal <sauravvishal8797@gmail.com>
2017-06-24 22:02:30 +0530

    Added starter code for catalogActivity

--- app/src/main/java/android/example/com/pets/CatalogActivity.java
+++ app/src/main/java/android/example/com/pets/CatalogActivity.java
@@ -1,13 +1,57 @@
 package android.example.com.pets;
 
+import static android.support.v7.widget.AppCompatDrawableManager.get;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private TextView mTextview;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        mTextview = (TextView) findViewById(R.id.text);
+
+        // Setup FAB to open EditorActivity
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        mFloatingActionButton.OnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                startActivity(new Intent(CatalogActivity.this, EditorActivity.class));
+            }
+        });
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
