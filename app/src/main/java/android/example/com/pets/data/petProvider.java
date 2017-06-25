b4d3ff907e57f8d31cfca48a9cabbff721c1b21e
Saurav Vishal <sauravvishal8797@gmail.com>
2017-06-25 21:11:54 +0530

    Added the query method in the Contentprovider class

--- app/src/main/java/android/example/com/pets/data/petProvider.java
+++ app/src/main/java/android/example/com/pets/data/petProvider.java
@@ -1,8 +1,11 @@
 package android.example.com.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,6 +18,15 @@ public class petProvider extends ContentProvider {
 
     private PetDbHelper mPetDbHelper;
 
+    private static final int PETS = 100;
+    private static final int PETS_ID = 101;
+
+    private static UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        uriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_NAME, 100);
+        uriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_NAME + "/", 101);
+    }
+
 
     @Override public boolean onCreate() {
 
@@ -25,7 +37,24 @@ public class petProvider extends ContentProvider {
     @Nullable @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1,
                         @Nullable String s1) {
-        return null;
+
+        SQLiteDatabase db = mPetDbHelper.getReadableDatabase();
+        Cursor cursor = null;
+        int match = uriMatcher.match(uri);
+        switch (match){
+            case PETS:
+
+                break;
+
+            case PETS_ID:
+                s = PetContract.PetEntry.PET_ID_COL + "=?";
+                strings1 = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(PetContract.PetEntry.TABLE_NAME, null, s, strings1, null, null, null);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown uri" + uri);
+        }
+        return cursor;
     }
 
     @Nullable @Override public String getType(@NonNull Uri uri) {
